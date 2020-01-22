df2fafc9808bdd2943356aec420afb42794bf089
Antonio Mora <antoniomora@amora-pk.local>
2020-01-22 12:58:30 -0600

    I had to solve all the stupid things I done to my code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { Link1Component } from './pages/link1/link1.component';
+import { Link2Component } from './pages/link2/link2.component';
+import { Link3Component } from './pages/link3/link3.component';
+import { Link4Component } from './pages/link4/link4.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo: 'link1', pathMatch: 'full'},
+  {path: 'link1', component: Link1Component, data: {progress: 'link1'}},
+  {path: 'link2', component: Link2Component, data: {progress: 'link4'}},
+  {path: 'link3', component: Link3Component, data: {progress: 'link3'}},
+  {path: 'link4', component: Link4Component, data: {progress: 'link4'}},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Link1Component } from './pages/link1/link1.component';
+import { Link2Component } from './pages/link2/link2.component';
+import { Link3Component } from './pages/link3/link3.component';
+import { Link4Component } from './pages/link4/link4.component';
+import { LoaderComponent } from './shared/loader/loader.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Link1Component,
+    Link2Component,
+    Link3Component,
+    Link4Component,
+    LoaderComponent
   ],
   imports: [
     BrowserModule,
